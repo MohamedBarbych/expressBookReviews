0ef7c6c2276721840efe53a8313542d5e6d0a6dd
MohamedBarbych <146338565+MohamedBarbych@users.noreply.github.com>
2024-07-27 20:58:42 +0100

    latest

--- final_project/index.js
+++ final_project/index.js
@@ -13,14 +13,12 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 
 // Authentication middleware for protected routes
 app.use("/customer/auth/*", function auth(req, res, next) {
-    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]; // Get token from the header
-
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
     if (!token) {
         return res.status(403).json({ message: "No token provided." });
     }
 
-    // Verify the token
-    jwt.verify(token, "your_secret_key", (err, decoded) => { // Replace with your actual secret key
+    jwt.verify(token, "", (err, decoded) => {
         if (err) {
             return res.status(403).json({ message: "Failed to authenticate token." });
         }
